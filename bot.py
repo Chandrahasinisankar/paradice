4bf707b9f3e50ec62d159054b36b2ecaa0e1b182
Chandrahasinisankar <115622757+Chandrahasinisankar@users.noreply.github.com>
2023-06-13 19:12:27 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ async def send_message(message, user_message, is_private):
 
 
 def run_discord_bot():
-    TOKEN = 'MTEwOTM3MjgwMDgwMTcxNDE4Ng.Gh13j4.6FFxSx7h-sON40eebK23fhyCvVyfRvMixEzUdY'
+    TOKEN = 'discord token'
     intents = discord.Intents.default()
     intents.message_content = True
     client = discord.Client(intents = intents)
